851df3face51b4bc2eb3302fdef73900ca6c8ce2
drincon2 <drincon2@miners.utep.edu>
2020-10-28 22:52:50 -0600

    Initial producer-consumer system

--- ThreadQueue/convertFrames.py
+++ ThreadQueue/convertFrames.py
@@ -4,15 +4,93 @@ from threading import Thread, Lock, Semaphore
 from threadedQueue import Queue
 import cv2
 import os
+import displayFrames
 
 # Queue for producer and consumer
-queue = Queue()
+convertQueue = Queue()
+# Queue for consumer and displayFrames consumer
+sendQueue = displayFrames.ProducerThread(Queue())
+# Output dir
+outputDir = 'frames'
+
 
 # Producer. Read frames from file.
 class ProducerThread(Thread):
+   # Extract frames from file, and read frames
    def run(self):
+      global convertQueue, outputDir
+      
+      # File
+      clipFileName = 'clip.mp4'
+      # Initialize frame count
+      count = 0
+      # Open the video clip
+      vidcap = cv2.VideoCapture(clipFileName)
+
+      # Create the output directory if it doesn't exist
+      if not os.path.exists(outputDir):
+         print(f"Output directory {outputDir} didn't exist, creating")
+         os.makedirs(outputDir)
+
+      # Read one frame
+      success,image = vidcap.read()
+
+      print(f'Reading frame {count} {success}')
+      while success and count < 72:
+
+         # Write the current frame out as a jpeg image
+         cv2.imwrite(f"{outputDir}/frame_{count:04d}.bmp", image)   
+         success,image = vidcap.read()
+         print(f'Reading frame {count}')
+         
+         # Pass current frame to convertQueue
+         convertQueue.enqueue(f"{outputDir}/frame_{count:04d}.bmp")
+         # TODO use semaphore to signal consumer
+         
+         count += 1
 
 
 # Consumer. Convert frames to grayscale and send to next producer-consumer.
-class ConsumerThread(Thread):
+class ConsumerThread(Thread):   
+   # Get read frame from queue
+   def getFrame(self, frames):
+      global convertQueue
+      return convertQueue.dequeue()
+   
+   # Convert frame to grayscale  
    def run(self):
+      global convertQueue, outputDir, sendQueue
+      # TODO use semaphore to get signal from Producer
+
+      # Initialize frame count
+      count = 0
+
+      # Get the next frame file name
+      inFileName = self.getFrame()
+
+      # Load the next file
+      inputFrame = cv2.imread(inFileName, cv2.IMREAD_COLOR)
+
+      while inputFrame is not None and count < 72:
+         print(f'Converting frame {count}')
+
+         # Convert the image to grayscale
+         grayscaleFrame = cv2.cvtColor(inputFrame, cv2.COLOR_BGR2GRAY)
+    
+         # Generate output file name
+         outFileName = f'{outputDir}/grayscale_{count:04d}.bmp'
+
+         # Write output file
+         cv2.imwrite(outFileName, grayscaleFrame)
+         # Send converted frame to displayFrames producer queue
+         sendQueue.ProducerThread.enqueue(outFileName)
+         # TODO use semaphore to signal displayFrames producer
+
+         count += 1
+
+         # TODO use semaphore to get signal from Producer
+         # Generate input file name for the next frame
+         inFileName = self.getFrame()
+    
+         # Load the next frame
+         inputFrame = cv2.imread(inFileName, cv2.IMREAD_COLOR)
--- ThreadQueue/displayFrames.py
+++ ThreadQueue/displayFrames.py
@@ -4,18 +4,54 @@ from threading import Thread, Lock, Semaphore
 from threadedQueue import Queue
 import cv2
 import time
-import numpy as np
-import base64
-import os
 
 # Queue for producer and consumer
-queue = Queue()
+displayQueue = Queue()
 
 # Producer. Extract converted frames from convertFrames.
 class ProducerThread(Thread):
+   def __init__(self, q):
+      self.queue = q
+      
    def run(self):
+      global displayQueue
+      # TODO use semaphore to get signal from convertFrames consumer
+      
+      # initialize frame count
+      count = 0
 
+      # Generate the filename for the first frame 
+      frameFileName = self.queue.dequeue()
+      # Pass current converted frame to displayQueue
+      displayQueue.enqueue(frameFileName)
+      # TODO use semaphore to signal consumer 
+      
 
 # Consumer. Display extracted frames.
 class ConsumerThread(Thread):
    def run(self):
+      global displayQueue
+      # TODO use semaphore to get signal from producer
+      # load the frame
+      frame = cv2.imread(displayQueue.dequeue())
+
+      while frame is not None:
+    
+         print(f'Displaying frame {count}')
+         # Display the frame in a window called "Video"
+         cv2.imshow('Video', frame)
+
+         # Wait for 42 ms and check if the user wants to quit
+         if cv2.waitKey(frameDelay) and 0xFF == ord("q"):
+            break    
+    
+         # get the next frame filename
+         count += 1
+         # TODO use semaphore to get signal from producer
+         frameFileName = displayQueue.dequeue()
+
+         # Read the next frame file
+         frame = cv2.imread(frameFileName)
+
+      # make sure we cleanup the windows, otherwise we might end up with a mess
+      cv2.destroyAllWindows()
